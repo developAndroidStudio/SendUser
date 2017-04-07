82949251495c9bb16a775940d3f2e0306525f650
CURSO_2 <isc_angelfierro.develop@outlook.es>
2017-04-07 12:07:50 -0500

    Se ofusco el código

--- app/src/main/java/mx/com/develop/senduser/MainActivity.java
+++ app/src/main/java/mx/com/develop/senduser/MainActivity.java
@@ -22,13 +22,16 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //<editor-fold desc="VARIABLES DE INSTANCIA">
     TextView tv_nombre, tv_telefono;
     ImageView iv_contacto;
     Button btn_seleccionar_contacto, btn_enviar;
 
     public static final int PICK_CONTACT_REQUEST = 1;
     private Uri contactUri;
+    //</editor-fold>
 
+    //<editor-fold desc="MÉTODO CALLBACK onCreate()">
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +39,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         inicializarComponentesUI();
         inicializarSetOnclickListener();
     }
+    //</editor-fold>
 
+    //<editor-fold desc="MÉTODO inicializarComponentesUI()">
     private void inicializarComponentesUI() {
         tv_nombre = (TextView) findViewById(R.id.tv_nombre);
         tv_telefono = (TextView) findViewById(R.id.tv_telefono);
@@ -44,35 +49,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_seleccionar_contacto = (Button) findViewById(R.id.btn_seleccionar_contacto);
         btn_enviar = (Button) findViewById(R.id.btn_enviar);
     }
+    //</editor-fold>
 
+    //<editor-fold desc="CONTROLADOR DE ENVENTOS setOnclickListener">
     private void inicializarSetOnclickListener() {
         btn_seleccionar_contacto.setOnClickListener(this);
         btn_enviar.setOnClickListener(this);
     }
+    //</editor-fold>
 
+    //<editor-fold desc="CONTROLADOR DE EVENTOS onClick(View v">
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+
             case R.id.btn_seleccionar_contacto:
                 actionPickContactos();
                 break;
+
             case R.id.btn_enviar:
                 sendMessage();
                 break;
         }
-
     }
+    //</editor-fold>
 
+    //<editor-fold desc="MÉTODO actionPickContactos()">
     public void actionPickContactos() {
 
-    /* Crear un intent para seleccionar un contacto del dispositivo */
+        /* Crear un intent para seleccionar un contacto del dispositivo */
         Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
 
- /*Iniciar la actividad esperando respuesta a travésdel canal PICK_CONTACT_REQUES */
+       /*Iniciar la actividad esperando respuesta a través del canal PICK_CONTACT_REQUES */
         startActivityForResult(i, PICK_CONTACT_REQUEST);
     }
+    //</editor-fold>
 
-
+    //<editor-fold desc=" MÉTODO onActivityResult()">
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == PICK_CONTACT_REQUEST) {
             if (resultCode == RESULT_OK) {
@@ -83,67 +96,74 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
     }
+    //</editor-fold>
 
-
+    //<editor-fold desc="MÉTODO extraerContacto(Uri uri">
     private void extraerContacto(Uri uri) {
+
         tv_nombre.setText(getName(uri));
         tv_telefono.setText(getPhone(uri));
         iv_contacto.setImageBitmap(getPic(uri));
     }
+    //</editor-fold>
 
+    //<editor-fold desc="MÉTODO getName(Uri uri)">
     private String getName(Uri uri) {
 
-    /* Valor a retornar */
+        /* Valor a retornar */
         String name = null;
 
-     /* Obtener una instancia del Content Resolver*/
+        /* Obtener una instancia del Content Resolver*/
         ContentResolver contentResolver = getContentResolver();
 
-    /*Cursor para recorrer los datos de la consulta*/
+        /*Cursor para recorrer los datos de la consulta*/
         Cursor c = contentResolver.query(
                 uri, new String[]{ContactsContract.Contacts.DISPLAY_NAME}, null, null, null);
 
-    /* Consultando el primer y único resultado elegido */
+        /* Consultando el primer y único resultado elegido */
         if (c.moveToFirst()) {
             name = c.getString(0);
         }
 
-    /*Cerramos el cursor*/
+        /*Cerramos el cursor*/
         c.close();
 
         return name;
     }
+    //</editor-fold>
 
+    //<editor-fold desc="MÉTODO getPhone(Uri uri)">
     private String getPhone(Uri uri) {
-    /* Variables temporales para el id y el teléfono */
+
+        //<editor-fold desc="Variables temporales para el id y el teléfono ">
         String id = null;
         String phone = null;
+        //</editor-fold>
 
-    /* PRIMERA CONSULTA  Obtener el _ID del contacto  */
-        Cursor contactCursor = getContentResolver().query(
-                uri, new String[]{ContactsContract.Contacts._ID},
-                null, null, null);
+        //<editor-fold desc="PRIMERA CONSULTA  Obtener el _ID del contacto ">
+        Cursor contactCursor = getContentResolver().query( uri, new String[]{ContactsContract.Contacts._ID},null, null, null);
 
         if (contactCursor.moveToFirst()) {
             id = contactCursor.getString(0);
         }
         contactCursor.close();
+        //</editor-fold>
 
-    /*  SEGUNDA CONSULTA  Sentencia WHERE para especificar que solo deseamos
-    números de telefonía móvil  */
-
+        //<editor-fold desc="SEGUNDA CONSULTA  Sentencia WHERE para especificar que solo deseamos números de telefonía móvil">
         String selectionArgs =
                 ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ? AND " +
                         ContactsContract.CommonDataKinds.Phone.TYPE + "= " +
                         ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE;
+        //</editor-fold>
 
-    /*  Obtener el número telefónico */
+        //<editor-fold desc="Obtener el número telefónico">
         Cursor phoneCursor = getContentResolver().query(
                 ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                 new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
                 selectionArgs,
                 new String[]{id},
                 null);
+        //</editor-fold>
 
         if (phoneCursor.moveToFirst()) {
             phone = phoneCursor.getString(0);
@@ -152,22 +172,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         return phone;
     }
+    //</editor-fold>
 
+    //<editor-fold desc="MÉTODO getPic(Uri uri)">
     private Bitmap getPic(Uri uri) {
-    /*Foto del contacto y su id  */
+
+        /*Foto del contacto y su id  */
         Bitmap photo = null;
         String id = null;
 
-        /************* CONSULTA ************/
+        //<editor-fold desc="CONSULTA">
         Cursor contactCursor = getContentResolver().query(
                 uri, new String[]{ContactsContract.Contacts._ID}, null, null, null);
+        //</editor-fold>
 
         if (contactCursor.moveToFirst()) {
             id = contactCursor.getString(0);
         }
         contactCursor.close();
 
-    /* Usar el método de clase openContactPhotoInputStream()*/
+        /* Usar el método de clase openContactPhotoInputStream()*/
         try {
             InputStream input =
                     ContactsContract.Contacts.openContactPhotoInputStream(
@@ -177,7 +201,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                     Long.parseLong(id))
                     );
             if (input != null) {
-            /*Dar formato tipo Bitmap a los bytes del BLOB correspondiente a  la foto */
+                /*Dar formato tipo Bitmap a los bytes del BLOB correspondiente a  la foto */
                 photo = BitmapFactory.decodeStream(input);
                 input.close();
             }
@@ -185,22 +209,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         return photo;
     }
+    //</editor-fold>
 
-
+    //<editor-fold desc="MÉTODO sendMessage()">
     public void sendMessage() {
 
-    /* Creando el gestor de mensajes*/
+        /* Creando el gestor de mensajes*/
         SmsManager smsManager = SmsManager.getDefault();
 
-    /*Enviando el mensaje*/
+        /*Enviando el mensaje*/
         if (contactUri != null) {
-            smsManager.sendTextMessage(String.valueOf((contactUri)), null,
-                    "¡Estas aprendiendo a Desarrollar en Android!", null, null);
+            smsManager.sendTextMessage(String.valueOf((contactUri)), null, "¡Estas aprendiendo a Desarrollar en Android!", null, null);
 
             Toast.makeText(this, "Mensaje Enviado", Toast.LENGTH_LONG).show();
         } else
             Toast.makeText(this, "Selecciona un contacto primero", Toast.LENGTH_LONG).show();
     }
+    //</editor-fold>
 
 
 }
